458d80baefad8c4d8db5569988df63cca37c75b8
Dean <dkunjravia@gmail.com>
2019-07-16 16:26:11 -0400

    dding multiple components

--- quiz/frontend/src/app/app.module.ts
+++ quiz/frontend/src/app/app.module.ts
@@ -4,20 +4,29 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatInputModule, MatCardModule, MatListModule} from '@angular/material';
 import {FormsModule} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
 import { ApiService } from './api.service';
 import { QuestionsComponent } from './questions.component';
+import { HomeComponent } from './home.component';
+
+const routes = [
+  { path: '', component: HomeComponent},
+  { path: 'question', component: QuestionComponent},
+  { path: 'questions', component: QuestionsComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent, QuestionComponent, QuestionsComponent
+    AppComponent, QuestionComponent, QuestionsComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatButtonModule,
     MatInputModule,
